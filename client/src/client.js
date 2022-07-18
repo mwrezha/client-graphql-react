5d38180ff89ede9aeb92ebf504865465e81dafa0
mwrezha <reza@machtwatch.co.id>
2022-07-18 14:08:40 +0700

    testing

--- client/src/client.js
+++ client/src/client.js
@@ -6,3 +6,33 @@ import gql from 'graphql-tag'
 /**
  * Create a new apollo client and export as default
  */
+
+
+const cache = new InMemoryCache()
+const link = new HttpLink({ uri: 'https://rickandmortyapi.com/graphql' })
+
+const client = new ApolloClient({
+  link,
+  cache,
+})
+
+const query = gql`
+    {
+      characters {
+        info {
+          count
+          pages
+        }
+        results {
+          id
+          name
+          type
+          image
+        }
+      }
+    }
+`;
+
+client.query({query}).then(result => console.log('characters', result))
+
+export default client;
